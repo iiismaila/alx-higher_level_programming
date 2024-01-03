8aa78c77c44211c2d29d3c725b077638b1877989
iiismaila <iiismaila@yahoo.com>
2024-01-03 07:53:07 -0800

    list_else3

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+vi 12-fizzbuzz.py 
+
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if (i % 15 == 0):
+            print("FizzBuzz ", end="")
+        elif (i % 3 == 0):
+            print("Fizz ", end="")
+        elif (i % 5 == 0):
+            print("Buzz ", end="")
+        else:
+            print("{:d} ".format(i), end="")
