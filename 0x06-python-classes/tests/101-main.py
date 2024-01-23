31f55e36a869feb1e6ac9b49168d1b93f2b00596
iiismaila <iiismaila@yahoo.com>
2024-01-23 04:09:51 -0800

    python_classes4

--- 0x06-python-classes/tests/101-main.py
+++ 0x06-python-classes/tests/101-main.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Test cases for the Square class.
+"""
+
 Square = __import__('101-square').Square
 
 my_square = Square(5, (0, 0))
@@ -8,3 +12,4 @@ print("--")
 
 my_square = Square(5, (4, 1))
 my_square.my_print()
+
