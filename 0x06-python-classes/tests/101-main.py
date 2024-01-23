21721b173714e7810f7e1fa4bc8870b0ac8593c7
iiismaila <iiismaila@yahoo.com>
2024-01-23 03:56:53 -0800

    python_classes2

--- 0x06-python-classes/tests/101-main.py
+++ 0x06-python-classes/tests/101-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-Square = __import__('101-square').Square
+from 101-square import Square
 
 my_square = Square(5, (0, 0))
 print(my_square)
@@ -8,3 +8,4 @@ print("--")
 
 my_square = Square(5, (4, 1))
 print(my_square)
+
